b4b1256bf233cba83df8bec92c9373bdf56c8191
Mustafa Atik <mustafaatik76@gmail.com>
2022-12-29 20:41:01 +0300

    utilities packages created

---
+++ src/test/java/utilities/BrowserFactory.java
@@ -0,0 +1,39 @@
+package utilities;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.safari.SafariDriver;
+
+public class BrowserFactory {
+
+    public static WebDriver getDriver(String browserType) {
+
+
+        if ((System.getProperty("os.name").equals("Windows 10") && browserType.equals("safari")) || (System.getProperty("os.name").equals("Mac") && browserType.equals("edge"))) {
+            return null;
+        }
+
+        if (browserType.equalsIgnoreCase("chrome")) {
+
+            WebDriverManager.chromedriver().setup();
+            return new ChromeDriver();
+
+        } else if (browserType.equalsIgnoreCase("firefox")) {
+            WebDriverManager.firefoxdriver().setup();
+            return new FirefoxDriver();
+        } else if (browserType.equalsIgnoreCase("edge")) {
+            System.setProperty("webdriver.edge.driver", "C://EdgeDriver.exe");
+            return new EdgeDriver();
+        }  else if(browserType.equals("Safari")) {
+            WebDriverManager.safaridriver().setup();
+            return new SafariDriver();
+        }    else {
+            System.out.println("Given browser type does not exist/or is not currently supported");
+            System.out.println("Driver = null");
+            return null;
+        }
+    }
+}
---
+++ src/test/java/utilities/StringUtility.java
@@ -0,0 +1,13 @@
+package utilities;
+
+public class StringUtility {
+
+    public static void verifyEquals(String expected, String actual){
+
+        if (expected.equals(actual)){
+            System.out.println("PASS");
+        }else {
+            System.out.println("FAIL"+ "Expected: "+expected+ "Actual: "+actual);
+        }
+    }
+}
